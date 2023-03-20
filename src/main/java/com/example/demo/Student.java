08fc14cc11ad1b8f80df0dbb35ad207a42347ef0
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-03-20 22:58:43 +0200

    First commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import lombok.Data;
+
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@EqualsAndHashCode
+@Data
+public class Student {
+	private long id;
+	private String firstName;
+	private String lastName;
+}
